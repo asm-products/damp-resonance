7cec081829c4bc2fe0f14e48dc818b2ac51536f6
normandmickey <normandmickey@gmail.com>
2014-05-21 22:08:20 -0400

    fixed oauth settings migration

--- db/migrate/20140520004857_create_facebook_oauth_settings.rb
+++ db/migrate/20140520004857_create_facebook_oauth_settings.rb
@@ -1,6 +1,6 @@
 class CreateFacebookOauthSettings < ActiveRecord::Migration
   def change
-    drop_table :facebook_oauth_settings
+#    drop_table :facebook_oauth_settings
     create_table :facebook_oauth_settings do |t|
       t.string :access_token
       t.integer :user_id
