a889f8aaf18d5b9f90836ce79c2cca51e54e95fb
normandmickey <normandmickey@gmail.com>
2014-05-22 22:58:14 -0400

    added facebook link to show ad

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,7 +17,7 @@ class User < ActiveRecord::Base
     user.uid = auth.uid
     user.email = auth.info.email
     user.password = Devise.friendly_token[0,20]
-    user.facebook_url = auth.info.link   
+    user.url = auth.extra.raw_info.link   
   end
 end  
 
